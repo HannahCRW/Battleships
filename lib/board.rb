e1384ee50a1ed9894208314effcf5609dfc00500
Hannah <hannah_wight@hotmail.com>
2014-03-10 13:54:24 +0000

    Setup player and board spec and lib files

---
+++ lib/board.rb
@@ -0,0 +1,18 @@
+class Board
+	def initialize player
+		@player = player
+	end
+
+	def owner
+		@player.name
+	end
+
+	def register_shot at_coordinates
+	end
+
+	def rows
+	end
+
+	def opponent_view
+	end
+end
---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player
+
+	def initialize name
+	end
+
+	def has_ships_still_floating?
+	end
+
+	def shoot(at_coordinates, opponent_board)
+	end
+end
