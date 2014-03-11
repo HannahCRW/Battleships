9683373e4677529601f78298ac0a1bd5e2b203b3
Toby Retallick <toby.retallick@gmail.com>
2014-03-11 14:43:09 +0000

    Fixed pesky infinite loop

--- lib/board.rb
+++ lib/board.rb
@@ -17,6 +17,7 @@ class Board
 		@player = player
 		@empty_board = Array.new(10) { Array.new(10, "") }
 		@standard_ships = [4,3,3,2,2,2,1,1,1,1]
+		@ships = 0
 	end
 
 	def owner
@@ -54,44 +55,59 @@ class Board
 	# end
 
 	def add_ships
-		@ships = 0
-		until @ships == 10 do
+		until @ships >= 10 do
 			add_horizontal_ship(rand(9),rand(9),ship_size)
-			@ships += 1
 			add_vertical_ship(rand(9),rand(9),ship_size)
-			@ships += 1
 		end
 		@ships
 	end
 
 	def add_horizontal_ship(x,y,size)
 		i = 0
-		x = 9 - size if x + size >= 9
-		# if rows[y][x] != "s" # skip to next
+		x = 9 - size if x + size >= 9 # making sure ship does not go over board edge
+		if rows[y][x] == "" && @ships < 10
 			while i < size do
 				rows[y][x] = "s"
-				i += 1
-				x += 1
+				i += 1; x += 1
 			end
-		# else
-		# 	@ships -= 1
-		# 	add_ships
-		# end
+			@ships += 1
+		end
 	end
 
 	def add_vertical_ship(x,y,size)
 		i = 0
 		y = 9 - size if y + size >= 9
-		# if rows[y][x] != "s" # skip to next
+		if rows[y][x] == "" && @ships < 10
 			while i < size do
 				rows[y][x] = "s"
-				i += 1
-				y += 1
+				i += 1; y += 1
 			end
-		# else
-		# 	@ships -= 1
-		# 	add_ships
-		# end
+			@ships += 1
+		end
 	end
+
+	# def check_x_space(x,y,size)
+	# 	i = 0
+	# 	is_there_space = false
+	# 	while i < size do
+	# 		rows[y][x] == "" ? is_there_space = true : is_there_space = false
+	# 		break if is_there_space == false
+	# 		i += 1; x += 1
+	# 	end
+	# 	is_there_space
+	# end
+
+	# def check_y_space(x,y,size)
+	# 	i = 0
+	# 	is_there_space = false
+	# 	while i < size do
+	# 		rows[y][x] == "" ? is_there_space = true : is_there_space = false
+	# 		break if is_there_space == false
+	# 		i += 1; y += 1
+	# 	end
+	# 	is_there_space
+	# end
+
+
 
 end
